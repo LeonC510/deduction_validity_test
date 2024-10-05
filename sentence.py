37992c4c0cb1ee0138b08aa13df514715ab5c590
Tianxiao Cao <tianxiao_cao@outlook.com>
2024-10-05 00:46:35 -0700

    Finished test_value_possible()

--- sentence.py
+++ sentence.py
@@ -2,11 +2,11 @@ from logical_connectives import reverse_truth_table
 
 
 class Sentence:
-    # TODO: Iterate to the next truth value combo each time this function is called, or iterate all combos each time
-    #  this function is called?
     first_test_value_possible = True
     last_truth_value_req = False
+    last_sentence_req = {"": False}
     truth_value_combo_iterator = 0
+
     def test_value_possible(self, truth_value: bool, sentence_reqs: dict[str, bool]) -> (bool, dict[str, bool]):
         """Test whether a given truth value is possible
 
@@ -16,41 +16,60 @@ class Sentence:
         fulfilled.
         """
         first_test_value_possible = False
+        test_result = False
 
         if not first_test_value_possible:
-            if truth_value != self.last_truth_value_req:
+            if truth_value != self.last_truth_value_req or self.last_sentence_req != sentence_reqs:
                 self.truth_value_combo_iterator = 0
-            else:
-                self.truth_value_combo_iterator += 1
+
+        self.last_truth_value_req = truth_value
+        self.last_sentence_req = sentence_reqs
 
         additional_sentence_reqs = dict()
-        truth_value_combo = reverse_truth_table[self.connective][truth_value][self.truth_value_iterator]
-        for sentence_index, sub_sentence in enumerate(self.sub_sentences):
-            # If the type of `sentence` is a non-empty string, it is an atomic sentence.
-            if type(sub_sentence) == str:
-                # If `sub_sentence` is empty, it is only a placeholder, and we can ignore this sub-sentence
-                if sub_sentence == "":
-                    continue
+        for truth_value_combo in reverse_truth_table[self.connective][truth_value][self.truth_value_combo_iterator:]:
+            self.truth_value_combo_iterator += 1
+            additional_sentence_reqs.clear()
+            for sentence_index, sub_sentence in enumerate(self.sub_sentences):
+                # If the type of `sentence` is a non-empty string, it is an atomic sentence.
+                if type(sub_sentence) == str:
+                    # If `sub_sentence` is empty, it is only a placeholder, and we can ignore this sub-sentence
+                    if sub_sentence == "":
+                        test_result = True
+                        continue
 
-                # If there is no requirements for this atomic sentence, we note the current truth value combo as a
-                # new requirement to be returned to the upper-level function.
-                if sentence_reqs.get(sub_sentence) == None:
-                    additional_sentence_reqs.update({sub_sentence: truth_value_combo[sentence_index]})
-                    continue
-                # If there is a requirement for this atomic sentence, but it's the same as the truth value combo
-                # specifies, we keep this combo and test it on the next sub-sentence.
-                elif truth_value_combo[sentence_index] == sentence_reqs.get(sub_sentence):
-                    continue
-                # If there is a requirement for this atomic sentence, but it doesn't fit
+                    # If there is no requirements for this atomic sentence, we note the current truth value combo as a
+                    # new requirement to be returned to the upper-level function. We then continue to check this combo
+                    # for the next sub-sentence.
+                    if sentence_reqs.get(sub_sentence) == None:
+                        additional_sentence_reqs.update({sub_sentence: truth_value_combo[sentence_index]})
+                        test_result = True
+                        continue
+                    # If there is a requirement for this atomic sentence, but it's the same as the truth value combo
+                    # specifies, we keep this combo and test it on the next sub-sentence.
+                    elif (truth_value_combo[sentence_index] == {**sentence_reqs, **additional_sentence_reqs}
+                            .get(sub_sentence)):
+                        test_result = True
+                        continue
+                    # If there is a requirement for this atomic sentence, but it doesn't fit, then this truth value
+                    # combo doesn't work, and we'll try the next truth value combo.
+                    else:
+                        test_result = False
+                        break
+                # If the type of `sentence` is `Sentence`, it is not an atomic sentence, and we use recursion to drill
+                # one level deeper.
+                elif type(sub_sentence) == Sentence:
+                    (test_result, temp_additional_sentence_reqs) = sub_sentence.test_value_possible(truth_value_combo[sentence_index],
+                                                                   {**sentence_reqs, **additional_sentence_reqs})
+                    additional_sentence_reqs.update(temp_additional_sentence_reqs)
+                # If it's not either of the types, something is wrong
                 else:
-                    break
-            # If the type of `sentence` is `Sentence`, it is not an atomic sentence, and we use recursion to drill
-            # one level deeper.
-            elif type(sub_sentence) == Sentence:
-                sub_sentence.test_value_possible(truth_value_combo[sentence_index])
-            # If it's not either of the types, something is wrong
-            else:
-                raise Exception("Incorrect sentence type: " + str(type(sub_sentence)))
+                    raise Exception("Incorrect sentence type: " + str(type(sub_sentence)))
+            # When we have checked all sub-sentences for this truth value combo and found that this combo works, we
+            # break out of the truth value combo loop and return the result.
+            if test_result == True:
+                break
+
+        return test_result, additional_sentence_reqs
 
     # Find all atomic sentences in the sentence stack included in this sentence
     def get_atomic_sentences(self, atomic_sentences: set):
@@ -68,6 +87,7 @@ class Sentence:
             # If it's not either of the types, something is wrong
             else:
                 raise Exception("Incorrect sentence type: " + str(type(sub_sentence)))
+
     def __init__(self, left_sentence, connective, right_sentence):
         self.sub_sentences = [left_sentence, right_sentence]
         self.connective = connective
